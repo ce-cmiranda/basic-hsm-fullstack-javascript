2547ffbfac6770f1abc328dffda4e60298868857
Carlos Miranda <ce.carv.miranda@gmail.com>
2020-07-02 20:34:40 -0300

    exercício 6

---
+++ ex_6/myscripts.js
@@ -0,0 +1,45 @@
+function criaBaralho(){
+    let valores = [];
+    let cartas_simbolos = ["Às", "Valete", "Dama", "Rei"]
+    let nipes = ['copas', 'espadas', 'ouros', 'paus'];
+    let baralho = []
+
+    for (let i = 2; i<11 ;i++){
+        valores.push(i)
+    }
+    
+    for(let j in cartas_simbolos){
+        valores.push(cartas_simbolos[j])
+    }
+
+    for (let k in nipes){
+        for (l in valores){
+            baralho.push(valores[l] + " de "+nipes[k])
+        }
+    }
+
+    return baralho;
+}
+
+function adivinha(){
+    baralho = criaBaralho()
+    
+    adivinha = true;
+    while (adivinha){
+
+        index_carta = Math.floor(Math.random()*baralho.length)
+        alert(baralho[index_carta])
+        resp = prompt("Acertei a carta?");
+        baralho.splice(index_carta,1)
+        if (resp.toLowerCase() == "sim"){
+            adivinha = false;
+        }
+        if (baralho.length == 0){
+            adivinha = false;
+            alert("Acabaram as cartas!")
+        }
+
+    }
+} 
+
+adivinha()
