1daa922194dbea5d2389f77c65375674ad057db4
Carlos Miranda <ce.carv.miranda@gmail.com>
2020-07-04 11:43:40 -0300

    adiciona desmatricula

--- ex_7/myscripts.js
+++ ex_7/myscripts.js
@@ -13,33 +13,48 @@ class Aluno{
 }
 
 class Turma{
-    constructor(codigo, nome, descricao, turno, alunos_matriculados = []){
+    constructor(codigo, nome, descricao, turno, alunos_matriculados = {}){
         this.codigo = codigo;
         this.nome = nome;
         this.descricao = descricao;
         this.turno = turno;
-        this.alunos_matriculados = alunos_matriculados
+        this.alunos_matriculados = alunos_matriculados;
+        this.qtd_matriculas = Object.keys(alunos_matriculados).length;
     }
 
     matricular_aluno(Aluno) {
-        this.alunos_matriculados.push(Aluno);
+        this.qtd_matriculas++;
+        this.alunos_matriculados[this.qtd_matriculas] = Aluno;
+    }
+    desmatricular_aluno(numero_matricula){
+        delete this.alunos_matriculados[numero_matricula];
     }
 }
 
 aluno1 = new Aluno("099.099.099-99","Carlos", "10/10/2000", "Masculino","Maria")
 aluno2 = new Aluno("088.088.088-88", "Eduarda", "11/11/2010", "Feminino","Daiane", "Joao")
+aluno3 = new Aluno("088.088.088-88", "Miranda", "11/11/2010", "Masculino","Daiane", "Joao")
 
-turma1 = new Turma("1", "Turma 1", "Esta é uma turma de teste", "Matutino", [aluno1])
+turma1 = new Turma("1", "Turma 1", "Esta é uma turma de teste", "Matutino", {1: aluno1})
+
+
+
+turma1.matricular_aluno(aluno2)
+turma1.matricular_aluno(aluno3)
 
 console.log(turma1.alunos_matriculados)
 
-turma1.matricular_aluno(aluno2)
-
+turma1.desmatricular_aluno(2)
 let lista_matriculados = "";
 
-for (i in turma1.alunos_matriculados){
-    lista_matriculados = lista_matriculados + ("<li>CPF: "+turma1.alunos_matriculados[i].cpf+
-        " Nome:" +turma1.alunos_matriculados[i].nome+"</li>")
+
+
+console.log(turma1.alunos_matriculados)
+
+for (let key in turma1.alunos_matriculados){
+    lista_matriculados = lista_matriculados + ("<li>Matrícula: " + key +
+        " CPF: "+turma1.alunos_matriculados[key].cpf+
+        " Nome:" +turma1.alunos_matriculados[key].nome+"</li>")
 }
 
 document.write("<div>Alunos matriculados na "+turma1.nome+":<ul>"+lista_matriculados+"</ul></div>")
